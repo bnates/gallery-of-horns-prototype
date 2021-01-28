2343dc81eae9882b05c3f2296632d0c30d94ee0e
bnates <brian.nations@codefellows.com>
2021-01-28 11:07:26 -0800

    added stripped version

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import Header from './header.js';
+import Main from './main.js';
+import Footer from './footer.js';
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
